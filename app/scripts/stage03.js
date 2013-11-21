23f9ee878d2228de17b24bd820d32b5ce24b80d1
Roger Sobrado <rogers@criticalmass.com>
2013-11-21 08:13:19 -0600

    stage03 merge

--- app/scripts/stage03.js
+++ app/scripts/stage03.js
@@ -1,7 +1,6 @@
 //Of course I can code this more programatically, but this seems good to me.
 $().ready(function () {
     $('.send').click(function () {
-        alert(123123123);
         setTimeout(function () {
             $('#plate').removeClass('front');
             $('#container').removeClass('beginning');
@@ -12,11 +11,11 @@ $().ready(function () {
                     $('#container').addClass('fly_away_first');
                     setTimeout(function () {
                         $('#container').addClass('fly_away');
-                        setTimeout(function () {
-                            $('#plate').addClass('front');
-                            $('#container').removeClass('fly_away fly_away_first hover').addClass('beginning');
-                            $('.curvable').removeClass('curved');
-                        }, 3000);
+                        // setTimeout(function () {
+                        //     $('#plate').addClass('front');
+                        //     $('#container').removeClass('fly_away fly_away_first hover').addClass('beginning');
+                        //     $('.curvable').removeClass('curved');
+                        // }, 3000);
                     }, 600);
                 }, 2000);
             }, 2800);
